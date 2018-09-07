60b7c69674d9a820eec1d335c131f2bf2347fd24
Phong Dong <phongdongt@gmail.com>
2018-09-06 23:35:01 -0700

    make change to support any of three Models

--- lab1/gumball­java­typical/GumballMachine.java
+++ lab1/gumball­java­typical/GumballMachine.java
@@ -1,43 +1,86 @@
 
+
 public class GumballMachine
 {
 
     private int num_gumballs;
     private boolean has_quarter;
+    private static int inserted_coin = 0;
+    private static final int   QUARTER = 25;
+    private String machine_type;
+    //Quarters    
+    //TwoQuarters 
+    //AllCoins
 
-    public GumballMachine( int size )
+    public GumballMachine( int size, String machine_type)
     {
-        // initialise instance variables
+        // initialize instance variables
         this.num_gumballs = size;
         this.has_quarter = false;
+        this.machine_type = machine_type;
     }
 
     public void insertQuarter(int coin)
     {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
+    		System.out.println("Inserting coin...");
+    		inserted_coin += coin;
+    		switch (machine_type) {
+			case "Quarters":
+				
+				this.has_quarter = coin == QUARTER ? true : false;
+				break;
+			case "TwoQuarters":
+				this.has_quarter = inserted_coin >= 2*QUARTER ? true : false;
+				break;
+			case "AllCoins":
+				this.has_quarter = inserted_coin >= 2*QUARTER ? true : false;
+				break;
+			default:
+				break;
+		}
+       
     }
     
     public void turnCrank()
     {
 	    	if ( this.has_quarter )
 	    	{
-	    		if ( this.num_gumballs > 0 )
-	    		{
-	    			this.num_gumballs-- ;
-	    			this.has_quarter = false ;
-	    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
-	    		}
-	    		else
-	    		{
-	    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
-	    		}
+	    		switch (machine_type) {
+				case "Quarters":
+					if ( this.num_gumballs > 0 )
+			    		{
+			    			this.num_gumballs-- ;
+			    			this.has_quarter = false ;
+			    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
+			    		}
+			    		else
+			    		{
+			    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
+			    		}
+					break;
+				case "TwoQuarters":
+				case "AllCoins":
+					if (this.num_gumballs > 0) {
+						this.num_gumballs--;
+						inserted_coin -= 2*QUARTER;
+						this.has_quarter = inserted_coin >= 2*QUARTER ? true : false;
+						System.out.println( "Thanks for your quarters.  Gumball Ejected!" ) ;
+					} else {
+						System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
+					}
+				break;
+
+				default:
+					break;
+				}
+	    		
 	    	}
 	    	else 
 	    	{
-	    		System.out.println( "Please insert a quarter" ) ;
+	    		System.out.println( "Please insert more coin!" ) ;
 	    	}        
+    }
+    public String toString() {
+    		return "### This is " + this.machine_type + " Gumball Machine.\n";
     }
 }
--- lab1/gumball­java­typical/Main.java
+++ lab1/gumball­java­typical/Main.java
@@ -3,20 +3,57 @@
 public class Main {
 
 	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+		/*
+		GumballMachine gumballMachine = new GumballMachine(5,"Quarters");
 
 		System.out.println(gumballMachine);
-
 		gumballMachine.insertQuarter( 25 );
 		gumballMachine.turnCrank();
 
 		System.out.println(gumballMachine);
-
 		gumballMachine.insertQuarter( 25 );
 		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
+		
 		System.out.println(gumballMachine);
+		gumballMachine.insertQuarter( 25 );
+		gumballMachine.turnCrank();
+		*/
+		
+		/*
+		GumballMachine gumballMachine = new GumballMachine(5,"TwoQuarters");
+		System.out.println(gumballMachine);
+		
+		gumballMachine.insertQuarter(25);
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter(25);
+		gumballMachine.insertQuarter(25);
+		gumballMachine.insertQuarter(25);
+
+		gumballMachine.turnCrank();
+		gumballMachine.turnCrank();
+		*/
+		
+		GumballMachine gumballMachine = new GumballMachine(5,"AllCoins");
+		System.out.println(gumballMachine);
+		
+		gumballMachine.insertQuarter(10);
+		gumballMachine.insertQuarter(5);
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter(10);
+		gumballMachine.insertQuarter(25);
+		gumballMachine.insertQuarter(10);
+		
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter(10);
+		gumballMachine.insertQuarter(10);
+		gumballMachine.insertQuarter(10);
+		gumballMachine.insertQuarter(10);
+		gumballMachine.turnCrank();
+
+		
+
+		
+
+		
 	}
 }
